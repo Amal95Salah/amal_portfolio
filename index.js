12d461962aba028e1f84859b820ce37bc5cb66b1
amal <amal95salah@gmail.com>
2022-10-07 22:28:51 +0300

    use tailwind and add enhancment

---
+++ index.js
@@ -0,0 +1,33 @@
+function emailAlert() {
+    alert("your email has been sent")
+}
+
+function openFormResponsibility1() {
+    document.getElementById("responsibility1").style.display = "block";
+    document.getElementById("frontend").style.display = "none";
+}
+
+function closeFormResponsibility1() {
+    document.getElementById("responsibility1").style.display = "none";
+    document.getElementById("frontend").style.display = "block";
+}
+
+function openFormResponsibility2() {
+    document.getElementById("responsibility2").style.display = "block";
+    document.getElementById("design").style.display = "none";
+}
+
+function closeFormResponsibility2() {
+    document.getElementById("responsibility2").style.display = "none";
+    document.getElementById("design").style.display = "block";
+}
+
+function openFormResponsibility3() {
+    document.getElementById("responsibility3").style.display = "block";
+    document.getElementById("internship").style.display = "none";
+}
+
+function closeFormResponsibility3() {
+    document.getElementById("responsibility3").style.display = "none";
+    document.getElementById("internship").style.display = "block";
+}
